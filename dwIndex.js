d100894bc8560d80826f7e05be4eeecd32c15bda
Jeison Moura <85846383+jeimoal@users.noreply.github.com>
2022-08-18 15:51:57 -0300

    Add files via upload

--- dwIndex.js
+++ dwIndex.js
@@ -1,5 +1,6 @@
 const nWallet = {
 
+
 init : () => {
 	newBtn = document.querySelector('#newBtn');
 	newBtn.addEventListener('click', nWallet.newWallet)
@@ -12,10 +13,47 @@ newWallet : () => {
 	cardDiv.setAttribute('class', 'container');
 	walletsDiv.appendChild(cardDiv);
 
+	//fig elements
+	binFig = document.createElement('img');
+	binFig.setAttribute('class', 'binFig')
+	cardDiv.appendChild(binFig);
+
+	trashFig = document.createElement('img');
+	trashFig.setAttribute('class', 'trashFig')
+	cardDiv.appendChild(trashFig);
+
+	circleFig = document.createElement('img');
+	circleFig.setAttribute('class', 'circleFig')
+	cardDiv.appendChild(circleFig);
+
 	//create top container
 	topContDiv = document.createElement('div');
 	topContDiv.setAttribute('class', 'topCont');
 	cardDiv.appendChild(topContDiv);
+
+	//TOP CONTAINER ELEMENTS
+
+	//top text
+	topTxtDiv = document.createElement('div');
+	topTxtDiv.setAttribute('class', 'topTxtDiv');
+	dwTxt = document.createElement('p');
+	dwTxt.textContent = 'digital wallet';
+	mwTxt = document.createElement('p');
+	mwTxt.textContent = 'my wallet';
+	topTxtDiv.appendChild(dwTxt);
+	topTxtDiv.appendChild(mwTxt);
+	topContDiv.appendChild(topTxtDiv);
+
+	//acc balance elements
+	accTxtDiv = document.createElement('div');
+	accTxtDiv.setAttribute('class', 'accTxtDiv');
+	blcTxt = document.createElement('p');
+	blcTxt.textContent = 'account balance';
+	valorTxt = document.createElement('p');
+	valorTxt.textContent = 'R$50.000';
+	accTxtDiv.appendChild(blcTxt);
+	accTxtDiv.appendChild(valorTxt);
+	topContDiv.appendChild(accTxtDiv);
 
 	//create mid container
 	midContDiv = document.createElement('div');

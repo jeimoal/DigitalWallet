33138a4235ae8cda66d648ce55ba4c7c6a8fce76
Jeison Moura <85846383+jeimoal@users.noreply.github.com>
2022-08-19 12:35:40 -0300

    Update dwIndex.js

--- dwIndex.js
+++ dwIndex.js
@@ -1,5 +1,17 @@
 const nWallet = {
+blc : 0, //balance value
+rsvd : 0, //reserved value
+limit : 0, //define limit per deposit
 
+addValueBtn : null, //acces html button
+wdwValueBtn : null, //acces html button
+svnValueBtn : null, //acces html button
+rsvdValueBtn : null, //acces html button
+
+addValueIpt : null, //acces html input
+wdwValueIpt : null, //acces html input
+svnValueIpt : null, //acces html input
+rsvdValueIpt : null, //acces html input
 
 init : () => {
 	newBtn = document.querySelector('#newBtn');
@@ -7,6 +19,9 @@ init : () => {
 },
 
 newWallet : () => {
+	nWallet.blc = 0;
+	nWallet.limit = 5000;
+
 	//create wallet container
 	walletsDiv = document.querySelector('#walletsDiv');
 	cardDiv = document.createElement('div');
@@ -50,7 +65,7 @@ newWallet : () => {
 	blcTxt = document.createElement('p');
 	blcTxt.textContent = 'account balance';
 	valorTxt = document.createElement('p');
-	valorTxt.textContent = 'R$50.000';
+	valorTxt.textContent = 'R$' + nWallet.blc;
 	display = document.createElement('img');
 	display.setAttribute('class', 'eyeFig');
 
@@ -96,14 +111,14 @@ newWallet : () => {
 	btmContDiv = document.createElement('div');
 	btmContDiv.setAttribute('class', 'btmCont');
 	cardDiv.appendChild(btmContDiv);
-	
+
 	//container elements
 	reserved = document.createElement('div');
 	reserved.setAttribute('class', 'reservedDiv');
 	rsvdTxt = document.createElement('p');
 	rsvdTxt.textContent = 'reserved available';
 	rsvdValue = document.createElement('p');
-	rsvdValue.textContent = 'R$50.000';
+	rsvdValue.textContent = 'R$' + nWallet.rsvd;
 	display = document.createElement('img');
 	display.setAttribute('class', 'eyeFig');
 	rsvdBtn = document.createElement('button');
@@ -115,7 +130,41 @@ newWallet : () => {
 	reserved.appendChild(rsvdBtn);
 	reserved.appendChild(display);
 	btmContDiv.appendChild(reserved);
-	
+
+	//passing buttons to atributtes
+	nWallet.addValueBtn = addBtn;
+	nWallet.wdwValueBtn = wdwBtn;
+	nWallet.svnValueBtn = svnBtn;
+	nWallet.rsvdValueBtn = rsvdBtn;
+
+	//add value function
+	addBtn.addEventListener('click', function () {
+	let vl = parseFloat(Number(prompt()));
+		//check if the entry is a number and is between the account deposit limit
+		if(Number.isFinite(vl) && vl <= nWallet.limit) {
+			nWallet.blc += vl;
+		}
+	valorTxt.textContent = 'R$' + nWallet.blc;
+	});
+
+	//withdraw value function
+	wdwBtn.addEventListener('click', function () {
+	let vl = parseFloat(Number(prompt()));
+		//check if the entry is a number and is between the account withdraw limit
+		if(Number.isFinite(vl) && vl <= nWallet.limit) {
+			//aditional validation to check if there's balance available
+			if(vl > nWallet.blc) {
+				alert('value not available');
+			} else {
+				nWallet.blc -= vl;
+			}
+		}
+	valorTxt.textContent = 'R$' + nWallet.blc;
+	});
+
+
+	svnBtn.addEventListener('click', nWallet.svnValueM);
+	rsvdBtn.addEventListener('click', nWallet.rsvdValueM);
 },
 
 }
